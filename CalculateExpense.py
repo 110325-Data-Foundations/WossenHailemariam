b2a5fb266e77811f1865f91af6f4a7ea2d740dd8
wossenhailemariam <wossen.k11@gmail.com>
2025-11-05 14:20:11 -0600

    Add dictionary

--- CalculateExpense.py
+++ CalculateExpense.py
@@ -7,15 +7,22 @@ sum = 0
 print("You can exit anytime, by writing 'exit'")
 print(" Please state number of items:")
 item_num = input()
+all_items = {}
 
 if item_num == "exit":
     exit()
+    # if not a number?
 
 for i in range(int(item_num)):
     print("item - ")
     item = input()
+    # if exit?
     print("cost -")
     cost = input()
+    all_items[item] = cost
     sum += int(cost)
     print(f"total is {sum}")
     
+for key, value in all_items.items():
+    print(key, value)
+print(f"   {sum}")
